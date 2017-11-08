714447bf2be5930698369cd535ee22fa975d3d6d
stan <1994wangning@gmail.com>
2017-11-08 17:31:30 +0100

    token connected

--- src/main/java/com/usping/kdsn/util/filter/AuthCheckFilter.java
+++ src/main/java/com/usping/kdsn/util/filter/AuthCheckFilter.java
@@ -9,8 +9,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.xml.bind.DatatypeConverter;
 import java.io.IOException;
 
-@WebFilter(filterName = "authCheck",urlPatterns = "/api/*")
-public class AuthCheckFilter implements Filter{
+@WebFilter(filterName = "authCheck", urlPatterns = "/api/*")
+public class AuthCheckFilter implements Filter {
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -19,18 +19,27 @@ public class AuthCheckFilter implements Filter{
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest req = (HttpServletRequest)servletRequest;
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+
+        System.out.println(req.getMethod());
+        if (req.getMethod().equals("OPTIONS")) {
+            filterChain.doFilter(servletRequest, servletResponse);
+            return;
+        }
+
         System.out.println(req.getHeader("token"));
         System.out.println("token parse:");
 
+        System.out.println(req.getHeader("Authorization"));
+
         Claims claims = Jwts.parser()
                 .setSigningKey(DatatypeConverter.parseBase64Binary("woshinidebaba"))
-                .parseClaimsJws(req.getHeader("token")).getBody();
+                .parseClaimsJws(req.getHeader("Authorization")).getBody();
 
         System.out.println(claims.getIssuer());
         System.out.println(claims.getSubject());
 
-        filterChain.doFilter(servletRequest,servletResponse);
+        filterChain.doFilter(servletRequest, servletResponse);
     }
 
     @Override
