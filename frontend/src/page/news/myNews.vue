70b2e6b181f4872c84adafac7a277983928d2ca9
MervynLeeeeeeee <mervynnc@163.com>
2018-07-05 21:06:58 +0800

    '修改评论列表样式'

--- frontend/src/page/news/myNews.vue
+++ frontend/src/page/news/myNews.vue
@@ -79,7 +79,7 @@
                     <div v-if="item.loading">
                       <ul v-for="comment in item.commentList" style="background: whitesmoke;">
                         <div>
-                          <span style="color: dimgrey;font-weight: bolder;font-size: 18px" >{{ comment.userName }}</span>: {{ comment.commentContent }}
+                          <span style="color: dimgrey;font-weight: bolder;font-size: 18px" >{{ comment.userName }}</span>:  {{ comment.commentContent }}
                           <span style="float: right;color: #d3d3d3">{{ comment.commentDate | timeFilter }}</span>
                         </div>
                         <span></span>
